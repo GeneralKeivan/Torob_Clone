970e0c75a69b92ba900addd649bfb09612abbfd3
Keivan Banan <simkeivanban@gmail.com>
2022-06-28 11:14:05 +0430

    Added to do comments to routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -12,3 +12,44 @@ const routing = () =>(
     </div>
 )
 export default routing;
+
+//We need to add routes for the following will add more
+//when i realize we need more routes
+
+
+// /accounts                                                     : shows the sign up or log in buttons
+
+
+// /accounts/login                                               : login page
+// /accounts/signup                                              : sign up page (can lead to login or main page)
+// (put a choice for customer, seller, admin in login)
+// (dont put admin in signup, admin is introduced manually)
+
+
+// /accounts/admin/:id
+// /accounts/customers/:id                                       : main customer page (happens after logging in) (sees all products, can click on products, can search products, can filter products)
+// /accounts/sellers/:id                                         : main seller page (happens after logging in) (can view products, add products, change products)
+
+
+// /accounts/customers/:id/product?searchedName                  : show searched product for customer (Dont know if we should use query like that) (search in all details entered for the products)
+// /accounts/customers/:id/product/filterName                    : show all products in the filtered category (filterName includes: mobiles, tablets, laptops)
+// /accounts/customers/:id/product/filterName/filterName2        : filterName2 is for the different brands(shown below)
+// /accounts/customers/:id/product/:id                           : shows specific product to customer
+
+
+// /accounts/sellers/:id/product/new                             : show create product page
+// /accounts/sellers/:id/product/change                          : show change product page
+
+
+//Filters:
+// mobiles/samsung
+// mobiles/xiaomi
+// mobiles/apple
+
+// tablets/samsung
+// tablets/xiaomi
+// tablets/apple
+
+// laptops/lenovo
+// laptops/asus
+// laptops/apple
