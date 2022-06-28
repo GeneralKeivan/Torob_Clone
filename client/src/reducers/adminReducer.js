2a07964ec4dbb60332651dd681017b884d972347
Keivan Banan <simkeivanban@gmail.com>
2022-06-28 12:04:52 +0430

    Added all base files needed (hopefully)

---
+++ client/src/reducers/adminReducer.js
@@ -0,0 +1,14 @@
+import {} from '../constants/ActionTypes'
+const initialState = {
+    admins : [],
+    admin : {}
+}
+const adminReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+}
+
+export default adminReducer;
+
+//Fill the reducers after getting the actions
