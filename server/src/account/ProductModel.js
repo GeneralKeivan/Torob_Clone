237a8e430f6d5575954397248035373c9947e004
Arman Hoseinmardi <armanhm79@gmail.com>
2022-06-30 20:52:22 +0430

    Different actions for product has been updated

--- server/src/account/ProductModel.js
+++ server/src/account/ProductModel.js
@@ -10,16 +10,17 @@ let productSchema = new Schema({
     model    : {type:String, default : null},
     brand    : {type:String, default : null},
     last_updated: {type:String, default : null},
-    minimum_price: {type:String, default : null},
-    maximum_price: {type:String, default : null},
+    cheap: {type:String, default : null},
+    expensive: {type:String, default : null},
     sellers    : [
         {
             name : {type:String, default : null},
-            phoneNumber: {type:String, default : null},
+            phone: {type:String, default : null},
             price: {type:String, default : null},
+            link:  {type:String, default : null}
         }
     ],
 },{timestamps : true});
 
-const preoduct = mongoose.model('product', sellerSchema);
-module.exports = seller;
+const product = mongoose.model('product', productSchema);
+module.exports = product;
